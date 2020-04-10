a82d2c10d40e3edd4f4a6cf2d358bc5012177582
croot22 <croot22@seas.upenn.edu>
2020-04-10 12:18:41 -0600

    Finished ranking method

--- src/RankForecast.java
+++ src/RankForecast.java
@@ -21,13 +21,26 @@ public class RankForecast{
 	}
 	
 	/**
- 	* This method takes in the class array list and activity and ranks the list according to the activity parameters.
- 	* @param 
+ 	* This method takes in the class array list and returns a new ranked list according to the activity parameters.
+ 	* @param null
  	*
  	*/
 	public ArrayList<DailyForecast> rankItems(){
 		
 		ArrayList<DailyForecast> rankedList = new ArrayList<DailyForecast>();
+		for(DailyForecast day: this.weatherList) {
+			calculateAverage(day);
+			rankedList.add(day);
+		}
+		rankedList.sort(null);
+		return rankedList;
+	}
+	
+	/**
+	 * This method calculates the average difference between weather variables from the chosen activity parameters
+	 * @param day
+	 */
+	private void calculateAverage(DailyForecast day) {
 		Integer diffTemperatureMax;
 		Integer diffTemperatureMin;
 
@@ -42,31 +55,22 @@ public class RankForecast{
 		
 		Double dayAverage = 0.0;
 		Double lowAverage = 0.0;
-
-		for(DailyForecast day: this.weatherList) {
-			diffTemperatureMax = Math.abs(day.getTemperatureMax() - activity.getBestTemperatureMax());
-			diffTemperatureMin = Math.abs(day.getTemperatureMin() - activity.getBestTemperatureMin());
-
-			// day part
-			diffPrecipChance = Math.abs(day.getPrecipChanceD() - activity.getBestPrecipChance());
-			diffCloudCover = Math.abs(day.getCloudCoverD() - activity.getBestCloudCover());
-			diffQpf = Math.abs(day.getQpfD() - activity.getBestQpf()) * 10;
-			diffQpfSnow = Math.abs(day.getQpfSnowD() - activity.getBestQpfSnow()) * 10;
-			diffTemperatureHeatIndex = Math.abs(day.getTemperatureHeatIndexD() - activity.getBestTemperatureHeatIndex());
-			diffTemperatureWindChill = Math.abs(day.getTemperatureWindChillD() - activity.getBestTemperatureWindChill());
-			//diffWindPhrase = Math.abs(day.getWindPhraseD() - activity.getBestWindPhrase());
-			
-			dayAverage = (diffTemperatureMax + diffTemperatureMin + diffPrecipChance + diffCloudCover + diffQpf + diffQpfSnow + diffTemperatureHeatIndex + diffTemperatureWindChill) / 8;
-			day.setAverage(dayAverage);
-			rankedList.add(day);
-		}
-		//need to figure out how to order
 		
-		return rankedList;
-	}
-	
-	private double getAverage() {
 		
+		diffTemperatureMax = Math.abs(day.getTemperatureMax() - activity.getBestTemperatureMax());
+		diffTemperatureMin = Math.abs(day.getTemperatureMin() - activity.getBestTemperatureMin());
+
+		// day part
+		diffPrecipChance = Math.abs(day.getPrecipChanceD() - activity.getBestPrecipChance());
+		diffCloudCover = Math.abs(day.getCloudCoverD() - activity.getBestCloudCover());
+		diffQpf = Math.abs(day.getQpfD() - activity.getBestQpf()) * 10;
+		diffQpfSnow = Math.abs(day.getQpfSnowD() - activity.getBestQpfSnow()) * 10;
+		diffTemperatureHeatIndex = Math.abs(day.getTemperatureHeatIndexD() - activity.getBestTemperatureHeatIndex());
+		diffTemperatureWindChill = Math.abs(day.getTemperatureWindChillD() - activity.getBestTemperatureWindChill());
+		//diffWindPhrase = Math.abs(day.getWindPhraseD() - activity.getBestWindPhrase());
+		
+		dayAverage = (diffTemperatureMax + diffTemperatureMin + diffPrecipChance + diffCloudCover + diffQpf + diffQpfSnow + diffTemperatureHeatIndex + diffTemperatureWindChill) / 8;
+		day.setAverage(dayAverage);
 		
 	}
 }
