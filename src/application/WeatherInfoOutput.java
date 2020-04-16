c220da5b31ee294100895c40328535f1bc4f975b
croot22 <croot22@seas.upenn.edu>
2020-04-16 16:09:21 -0600

    creating output method:

--- src/application/WeatherInfoOutput.java
+++ src/application/WeatherInfoOutput.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.ArrayList;
 
 /**
  * Outputs the weather information into txt file
@@ -8,5 +9,18 @@ package application;
  */
 public class WeatherInfoOutput {
 	
+	private ArrayList<DailyForecast> weatherOutputList;
+	OutdoorActivity chosenActivity;
+	
+	public WeatherInfoOutput(ArrayList<DailyForecast> weatherOutputList, OutdoorActivity chosenActivity) {
+		this.weatherOutputList = weatherOutputList;
+		this.chosenActivity = chosenActivity;
+	}
+	
+	public void outputWeatherInfo() {
+		RankForecast rank = new RankForecast(weatherOutputList, chosenActivity);
+		this.weatherOutputList = rank.getRankedList();
+		
+	}
 	
 }
